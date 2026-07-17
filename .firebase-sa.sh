ba8f0ceddf944fdf652ad2096a09910038a933b3
freddyfresko <pcd.dns@gmail.com>
2026-07-17 18:56:01 -0400

    chore: limpiar archivos no necesarios del repo

--- .firebase-sa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="C:/Users/Freddy/.config/firebase/sa-key.json"
-"/c/Users/Freddy/AppData/Local/Microsoft/WinGet/Packages/OpenJS.NodeJS.LTS_Microsoft.Winget.Source_8wekyb3d8bbwe/node-v24.16.0-win-x64/firebase" "$@"
